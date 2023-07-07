3043baa85683f30fe1bd3c2b481c20ba33dab7e1
Cathy Biwott <biwottcathy@gmail.com>
2023-07-07 17:37:35 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import Navbar from './Navbar';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Products from './Products';
+import ProductDetailsPage from './ProductDetails';
+import AddProductForm from './AddProductForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar/>
+<BrowserRouter>
+<Routes>
+  { <Route path='/Login' element ={<Login/>}></Route> }
+  { <Route path='ProductDetails' element={<ProductDetailsPage/>}/> }
+  { <Route path='/product/:productId' element ={<ProductDetailsPage/>}/> }
+  {<Route path='/Products' element ={<Products/>}/> }
+  { <Route path ='/AddProduct' element={<AddProductForm/>}/> }
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
